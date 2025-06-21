b2b53c85dad409c63789af4b40975778ac3cdc44
RaphaLK <77198909+RaphaLK@users.noreply.github.com>
2025-06-20 22:52:50 -0700

    fixed linker issues, overload _start

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MiniRustOS
  
+Inspired and Guided by Philipp Oppermann's Blog on OS Development in Rust
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,14 @@
 #![no_main]
 use core::panic::PanicInfo;
 
+// Override panic handler
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+// Disable name mangling -- C Calling convention
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop{}
+}
