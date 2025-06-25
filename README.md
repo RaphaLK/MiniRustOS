8e4e8c0bf82d5a0bf5602a9f07dbc0235fbb3f71
RaphaLK <77198909+RaphaLK@users.noreply.github.com>
2025-06-24 22:23:39 -0700

    finished kernel, QEMU bootable

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # MiniRustOS
  
 Inspired and Guided by Philipp Oppermann's Blog on OS Development in Rust
+
+## Booting in QEMU
+qemu-system-x86_64 -drive format=raw,file=target/x86_64-target/debug/bootimage-minirustos.bin
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![no_main]
+
 use core::panic::PanicInfo;
 
 // Override panic handler
@@ -7,10 +8,19 @@ use core::panic::PanicInfo;
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
+static HELLO: &[u8] = b"Hello World";
 
 // Disable name mangling -- C Calling convention
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    // Entry point function, named _start by default
-    loop{}
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
 }
