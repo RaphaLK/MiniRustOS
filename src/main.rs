ba1521d295c8cfe68373d500e14645eae375d2b0
RaphaLK <77198909+RaphaLK@users.noreply.github.com>
2025-07-13 22:46:17 -0700

    Proper print and println macros

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,6 @@ fn panic(_info: &PanicInfo) -> ! {
 // Disable name mangling -- C Calling convention
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    // let vga_buffer = 0xb8000 as *mut u8;
-
-    // for (i, &byte) in HELLO.iter().enumerate() {
-    //     unsafe {
-    //         *vga_buffer.offset(i as isize * 2) = byte;
-    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-    //     }
-    // }
-    vga_buffer::print_something();
-
+    println!("Hello World{}", "!");
     loop {}
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -106,18 +106,18 @@ impl Writer {
     }
 }
 
-pub fn print_something() {
-    use core::fmt::Write;
-    let mut writer = Writer {
-        column_position: 0,
-        color_code: ColorCode::new(Color::Yellow, Color::Black),
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
+// pub fn print_something() {
+//     use core::fmt::Write;
+//     let mut writer = Writer {
+//         column_position: 0,
+//         color_code: ColorCode::new(Color::Yellow, Color::Black),
+//         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+//     };
 
-    writer.write_byte(b'H');
-    writer.write_string("ello ");
-    write!(writer, "The numbers are {} and {}", 42, 1.0 / 3.0).unwrap();
-}
+//     writer.write_byte(b'H');
+//     writer.write_string("ello ");
+//     write!(writer, "The numbers are {} and {}", 42, 1.0 / 3.0).unwrap();
+// }
 
 use core::fmt;
 impl fmt::Write for Writer {
@@ -126,12 +126,30 @@ impl fmt::Write for Writer {
         Ok(())
     }
 }
-use lazy_static::lazy_static;
 
+use spin::Mutex;
+use lazy_static::lazy_static;
 lazy_static! {
-    pub static ref WRITER: Writer = Writer {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
         column_position: 0,
         color_code: ColorCode::new(Color::Yellow, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
+    });
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
 }
