a068cee5e2e63133e0b08f646cdff7e3f51f8214
RaphaLK <77198909+RaphaLK@users.noreply.github.com>
2025-07-24 15:42:12 -0700

    Double fault handler

--- src/main.rs
+++ src/main.rs
@@ -28,8 +28,9 @@ pub extern "C" fn _start() -> ! {
 
     minirustos::init();
 
-    x86_64::instructions::interrupts::int3();
-    
+    unsafe {
+        *(0xdeadbeef as *mut u8) = 42;
+    }
     #[cfg(test)]
     test_main();
     loop {}
