d946fd7bc46aaeee7cf792403a812ad1aca48e28
RaphaLK <77198909+RaphaLK@users.noreply.github.com>
2025-08-04 22:04:19 -0700

    PIC Initialization

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,8 @@ use core::panic::PanicInfo;
 pub fn init() {
   gdt::init();
   interrupts::init_idt();
+  unsafe { interrupts::PICS.lock().initialize(); }; // PIC Initialization
+  x86_64::instructions::interrupts::enable();
 }
 pub trait Testable {
     fn run(&self) -> ();
